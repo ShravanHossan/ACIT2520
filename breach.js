6a8fba506ca4178d183f1b8ec4a58ad582a10387
Shravan Hossan <Shravan1020@hotmail.com>
2019-03-30 15:08:59 -0700

    Breach done

--- breach.js
+++ breach.js
@@ -1,32 +1,49 @@
 const axios = require('axios');
 const crypto = require('crypto');
-
+const _ = require('lodash');
 
 const passwords_breach_lookup = (password) => {
     return new Promise((resolve, reject) =>  {
 
-        let hashed_password = crypto.createHash('sha1').update(JSON.stringify((password))).digest('hex').toUpperCase();
-        console.log(hashed_password);
-        console.log(hashed_password.substring(0, 5));
-        let hash_list = axios.get(`https://api.pwnedpasswords.com/range/${hashed_password.substring(0, 5)}`);
+        const hash = crypto.createHash('sha1');
+        try {
+            let data = hash.update(password, 'utf-8');
 
-        hash_list.then(result => {
-        let list = {Passwords: (result.data).split(`\r\n`)};
 
-        for (line in list.Passwords){
-            if (hashed_password === line.substring(5))
+            let hashed_password = data.digest('hex').toUpperCase();
 
-                console.log('success');
-            console.log(list.Passwords[line])
+
+            let hash_list = axios.get(`https://api.pwnedpasswords.com/range/${hashed_password.substring(0, 5)}`);
+
+            hash_list.then(result => {
+            let list = {Passwords: (result.data).split(`\r\n`)};
+
+
+            for (let line in list.Passwords){
+
+                if (hashed_password.substring(5, 40) === list.Passwords[line].substring(0, 35))
+
+                    resolve(`Password: ${password} was found ${list.Passwords[line].substring(36,50)} times (hash : ${hashed_password})`)
+            }
+
+            resolve(`${password} was not found`)
+
+
+
+        }).catch(err => {
+            reject("Error: ", err)
+        });
         }
-        // console.log("Result: ",list.Passwords)
-    }).catch(err => {
-        console.log("Error: ", err)
-    });
+        catch (e) {
+            reject(`Password ${password} is invalid`)
+        }
 })};
 
-passwords_breach_lookup("Password").then(result => {
-    console.log(result)
-}).catch(err => [
-    console.log(err)
-]);
+// passwords_breach_lookup("password").then(result => {
+//     console.log(result)
+// }).catch(err => [
+//     console.log(err)
+// ]);
+module.exports = {
+    passwords_breach_lookup
+};
