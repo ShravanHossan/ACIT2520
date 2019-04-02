83b2968394c200f4e50095e9f07052cc3b040fdf
guanvictor <44719163+guanvictor@users.noreply.github.com>
2019-04-01 17:19:08 -0700

    Add files via upload
    Added Promises to generator

--- generator.js
+++ generator.js
@@ -1,22 +1,35 @@
+//With Promises
 const generatePassword = () => {
-	var length = 20,
-		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-		password = "";
+    return new Promise(((resolve, reject) => {
+        var length = 20,
+            charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            password = "";
 
-	for (var i = 0; n = charset.length, i < length; i++) {
-		password += charset.charAt(Math.floor(Math.random() * n));
-	}
-	return password;
-}
+        for (var i = 0; n = charset.length, i < length; i++) {
+            password += charset.charAt(Math.floor(Math.random() * n));
+        }
+        resolve(password)
+    }))};
 
-console.log(generatePassword());
 
-// Easier way to do it using a generate-password module
-// const generator = require('generate-password');
+generatePassword().then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
+});
 
-// var password = generator.generate({
-// 	length: 20,
-// 	numbers: true
-// });
+// console.log(generatePassword());
 
-// console.log(password);
+// const generatePassword = () => {
+//     var length = 20,
+//         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//         password = "";
+//
+//     for (var i = 0; n = charset.length, i < length; i++) {
+//         password += charset.charAt(Math.floor(Math.random() * n));
+//     }
+//     return password;
+// }
+//
+// console.log(generatePassword());
+
