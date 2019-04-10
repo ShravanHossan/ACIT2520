f20b3937a14cb0e5eab204b307dfde3ccb6dbd3e
guanvictor <guanvictor@hotmail.com>
2019-04-09 17:24:49 -0700

    Database connection

--- public/utils.js
+++ public/utils.js
@@ -1,4 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+//
+// var _db = null;
+//
+// module.exports.getDb = function() {
+//     return _db;
+// };
+//
+// module.exports.init = function(callback) {
+//     MongoClient.connect('mongodb://localhost:27017/test', function (err, client) {
+//         if (err) {
+//             return console.log('Unable to connect to DB');
+//         }
+//         _db = client.db('test');
+//         console.log('Successfully connected to MongoDB server')
+//     });
+// };
+
 const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Spyder:r0PTfX3Z5lYsaLvs@cluster0-jogjo.gcp.mongodb.net/test?retryWrites=true";
 
 var _db = null;
 
@@ -6,22 +25,11 @@ module.exports.getDb = function() {
     return _db;
 };
 
-module.exports.init = function(callback) {
-    MongoClient.connect('mongodb://localhost:27017/test', function (err, client) {
-        if (err) {
-            return console.log('Unable to connect to DB');
-        }
-        _db = client.db('test');
-        console.log('Successfully connected to MongoDB server')
-    });
-};
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    if (err)
+        return err;
 
-//
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://Spyder:<password>@cluster0-jogjo.gcp.mongodb.net/test?retryWrites=true";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//     const collection = client.db("test").collection("accounts");
-//     // perform actions on the collection object
-//     client.close();
-// });
+    _db = client.db('acit');
+    console.log('Successfully connected to MongoDB server')
+});
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const utils = require('./public/utils');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
 const breach = require('./public/breach.js');
@@ -9,6 +11,11 @@ const manager = require('./public/manager.js');
 
 const port = process.env.PORT || 8080;
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -50,26 +57,50 @@ app.get('/manage', (request, response) => {
 	});
 });
 
-app.post('/manage', urlencodedParser, (request, response) => {
-	manager.add_password(request.body.username, request.body.url, request.body.password).then((message) => {
-		response.render('manager.hbs', {
-			title: 'Password Manager',
-			output: message
-		});
-	});
-});
+// app.post('/manage', urlencodedParser, (request, response) => {
+// 	manager.add_password(request.body.username, request.body.url, request.body.password).then((message) => {
+// 		rsponse.render('manager.hbs', {
+// 			title: 'Password Manager',
+// 			output: message
+// 		});
+// 	});
+// });
 
-app.get('/breach', (request, response) => {
-	response.render('breach.hbs', {
-		title: 'Breach'
+//Add an email, website, and password to the database
+app.post('/addAccount', function(request, response) {
+
+	var account = {
+		// email: request.body.email,
+		website: request.body.website,
+		password: request.body.password
+	};
+
+	var db = utils.getDb();
+	db.collection('accounts').insertOne(account, function (err, result) {
+		if (err) {
+			response.send('Unable to insert account');
+		}
+		response.send(JSON.stringify(result.ops, undefined, 2))
+		// response.send("Password Added Successfully.")
+		// response.render('manager.hbs', {
+		// 	title: 'Manager',
+		// 	output: message
+		// });
 	});
+
 });
 
 app.post('/breach', urlencodedParser, (request, response) => {
 	breach.passwords_breach_lookup(request.body.password).then((message) => {
 		response.render('breach.hbs', {
-			title: 'Breach',
-			output: message
+
+		});
+
+		app.get('/breach', (request, response) => {
+			response.render('breach.hbs', {
+				title: 'Breach',
+				output: message
+			});
 		});
 	}).catch((error) => {
 		response.render('breach.hbs', {
@@ -77,6 +108,14 @@ app.post('/breach', urlencodedParser, (request, response) => {
 			output: error
 		});
 	});
+});
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, database) {
+	if (err) {
+		return console.log('Unable to connect to DB');
+
+	}
+	database.close()
 });
 
 app.listen(port, () => {
